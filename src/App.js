c658496dba44288b57e68244777aa906c8c1b816
Baby Medusa <duartebaltazar10@gmail.com>
2022-03-31 22:19:00 -0300

    Enter

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome To Medusa
+          Welcome To Medusa. We believe we will be able to make an awesome app by coding it from scratch. Yes, learning everything in a week and launching the best token ever.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://t.me/medusalabs"
           target="_blank"
           rel="noopener noreferrer"
         >
-         We believe we will be able to make an awesome app by coding it from scratch. Yes, learning everything in a week and launching the best token ever.
-        </a>
+        Join our Telegram here
       </header>
     </div>
   );
